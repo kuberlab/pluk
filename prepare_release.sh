7612a001a0b069d66d65c24d3169e5854aaa8288
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2018-01-31 15:22:18 +0300

    Add prepare release script

---
+++ prepare_release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+version=$1
+
+if [ -z $version ];
+then
+  echo "Usage: prepare_release.sh <version>"
+  exit 1
+fi
+
+GOOS=linux go build -v -o kdataset ./cmd/kdataset
+tar -cvzf kdataset-linux-amd64-$version.tar.gz kdataset README.md
+
+GOOS=darwin go build -v -o kdataset ./cmd/kdataset
+tar -cvzf kdataset-darwin-amd64-$version.tar.gz kdataset README.md
+
+GOOS=windows go build -v -o kdataset.exe ./cmd/kdataset
+tar -cvzf kdataset-windows-amd64-$version.tar.gz kdataset.exe README.md
+
+rm kdataset kdataset.exe
+
