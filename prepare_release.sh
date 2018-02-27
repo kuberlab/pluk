870f776ee88c391d8f17923f4b7368dab3ec9fef
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2018-02-27 14:42:00 +0300

    Add flags

--- prepare_release.sh
+++ prepare_release.sh
@@ -8,13 +8,13 @@ then
   exit 1
 fi
 
-GOOS=linux go build -v -o kdataset ./cmd/kdataset
+GOOS=linux go build -v -ldflags="-s -w" -o kdataset ./cmd/kdataset
 tar -cvzf kdataset-linux-amd64-$version.tar.gz kdataset README.md
 
-GOOS=darwin go build -v -o kdataset ./cmd/kdataset
+GOOS=darwin go build -v -ldflags="-s -w" -o kdataset ./cmd/kdataset
 tar -cvzf kdataset-darwin-amd64-$version.tar.gz kdataset README.md
 
-GOOS=windows go build -v -o kdataset.exe ./cmd/kdataset
+GOOS=windows go build -v -ldflags="-s -w" -o kdataset.exe ./cmd/kdataset
 tar -cvzf kdataset-windows-amd64-$version.tar.gz kdataset.exe README.md
 
 rm kdataset kdataset.exe
