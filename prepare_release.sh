99d84029c3c8067929d4ce25beeacaebf451f339
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2018-04-20 23:48:46 +0300

    Fix build binaries for google storage

--- prepare_release.sh
+++ prepare_release.sh
@@ -9,9 +9,11 @@ then
 fi
 
 GOOS=linux go build -v -ldflags="-s -w -X main.VersionStr=$version" -o kdataset ./cmd/kdataset
+cp kdataset kdataset-linux
 tar -cvzf kdataset-linux-amd64-$version.tar.gz kdataset README.md
 
 GOOS=darwin go build -v -ldflags="-s -w -X main.VersionStr=$version" -o kdataset ./cmd/kdataset
+cp kdataset kdataset-osx
 tar -cvzf kdataset-darwin-amd64-$version.tar.gz kdataset README.md
 
 GOOS=windows go build -v -ldflags="-s -w -X main.VersionStr=$version" -o kdataset.exe ./cmd/kdataset
