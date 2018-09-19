074d718e7a523b464a6d6dd58a22a9b6d8980c5c
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2018-09-19 22:37:45 +0400

    More comfortable tests script

--- tests.sh
+++ tests.sh
@@ -4,12 +4,12 @@ RED="\033[0;31m"
 GREEN="\033[0;32m"
 NC='\033[0m' # No color
 
-go test -v github.com/kuberlab/pluk/...
+num=$(go test -v github.com/kuberlab/pluk/... | tee /dev/tty | grep RUN | wc -l)
 exit_code=$(echo $?)
 
 if [ $exit_code -eq 0 ]; then
-  echo -e "${GREEN}Tests passed${NC}"
+  echo -e "${GREEN}Run $num tests\nTests passed${NC}"
 else
-  echo -e "${RED}Tests failed${NC}"
+  echo -e "${RED}Run $num tests\nTests failed${NC}"
   exit $exit_code
 fi
