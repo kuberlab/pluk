1edfb0fbd279cc12a3286ecd7ef590e356ee1c3f
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2018-12-28 20:24:23 +0400

    Fix expiring messages

--- pkg/types/hub.go
+++ pkg/types/hub.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"sync"
+	"time"
 
 	"github.com/Sirupsen/logrus"
 )
@@ -10,15 +11,56 @@ type Hub struct {
 	lock         *sync.RWMutex
 	connections  map[*WebsocketClient]bool
 	queue        chan Message
-	lastMessages []Message
+	lastMessages *ExpiredMessages
 }
 
 const (
 	SaveLastMessageLimit = 5
+	expireTime           = time.Hour
 )
 
 type Message interface {
 	Type() string
+}
+
+type ExpiredMessages struct {
+	messages []Message
+	expires  []time.Time
+}
+
+func NewExpiredMessages() *ExpiredMessages {
+	return &ExpiredMessages{
+		expires:  make([]time.Time, 0),
+		messages: make([]Message, 0),
+	}
+}
+
+func (em *ExpiredMessages) AddMessage(message Message) {
+	length := len(em.messages)
+	expire := time.Now().Add(expireTime)
+	if length < SaveLastMessageLimit {
+		em.messages = append(em.messages, message)
+		em.expires = append(em.expires, expire)
+		return
+	}
+
+	em.messages = append(em.messages[:SaveLastMessageLimit-1], message)
+	em.expires = append(em.expires[:SaveLastMessageLimit-1], expire)
+}
+
+func (em *ExpiredMessages) CheckExpire() {
+	now := time.Now()
+	j := 0
+	for i := range em.expires {
+		if !now.After(em.expires[i]) {
+			// Delete
+			em.messages[j] = em.messages[i]
+			em.expires[j] = em.expires[i]
+			j++
+		}
+	}
+	em.messages = em.messages[:j]
+	em.expires = em.expires[:j]
 }
 
 func NewHub() *Hub {
@@ -26,7 +68,7 @@ func NewHub() *Hub {
 		lock:         &sync.RWMutex{},
 		connections:  make(map[*WebsocketClient]bool),
 		queue:        make(chan Message, 20),
-		lastMessages: make([]Message, 0),
+		lastMessages: NewExpiredMessages(),
 	}
 	go hub.eventLoop()
 
@@ -36,7 +78,8 @@ func NewHub() *Hub {
 func (h *Hub) Register(client *WebsocketClient) {
 	h.lock.Lock()
 	defer h.lock.Unlock()
-	h.PushMany(client, h.lastMessages)
+	h.lastMessages.CheckExpire()
+	h.PushMany(client, h.lastMessages.messages)
 	h.connections[client] = true
 }
 
@@ -78,13 +121,8 @@ func (h *Hub) Push(message Message) {
 }
 
 func (h *Hub) saveLastMessage(message Message) {
-	length := len(h.lastMessages)
-	if length < SaveLastMessageLimit {
-		h.lastMessages = append(h.lastMessages, message)
-		return
-	}
-
-	h.lastMessages = append(h.lastMessages[:SaveLastMessageLimit-1], message)
+	h.lastMessages.AddMessage(message)
+	h.lastMessages.CheckExpire()
 }
 
 func (h *Hub) pushMessage(message Message) {
