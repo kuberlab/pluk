43d6387aa8b762ec671bd41cc219cc301dceec02
Nikolay Mahotkin <nikolay.makhotkin@gmail.com>
2020-11-19 11:45:32 +0400

    Fix import automaxprocs

--- cmd/kdataset/kdataset.go
+++ cmd/kdataset/kdataset.go
@@ -11,7 +11,7 @@ import (
 	"github.com/kuberlab/pluk/cmd/kdataset/config"
 	"github.com/kuberlab/pluk/pkg/plukclient"
 	"github.com/spf13/cobra"
-	_ "github.com/uber-go/automaxprocs"
+	_ "go.uber.org/automaxprocs"
 )
 
 const (
